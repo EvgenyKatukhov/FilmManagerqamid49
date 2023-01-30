02bc32502e31c5b3e5a96c279b8e9c43fd31bc10
Evgeny Katukhov <e.katuhov@gmail.com>
2023-01-30 21:55:45 +0300

    SRS

--- src/main/java/ru/netology/domain/Poster.java
+++ src/main/java/ru/netology/domain/Poster.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class Poster {
+package ru.netology.domain;
+
+public class Poster {
+
+    private String title;
+    private int id;
+    private String genre;
+    private int limit;
+
+    public Poster(String title, int id, String genre) {
+        this.title = title;
+        this.id = id;
+        this.genre = genre;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
 }
+
