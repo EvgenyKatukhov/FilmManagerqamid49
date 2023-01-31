dbb9ff1e2ae9e106872ecf03419e0bfc79bbcdc3
Evgeny Katukhov <e.katuhov@gmail.com>
2023-01-31 20:05:29 +0300

    Tests

--- src/main/java/ru/netology/domain/Poster.java
+++ src/main/java/ru/netology/domain/Poster.java
@@ -7,6 +7,7 @@ public class Poster {
     private String genre;
     private int limit;
 
+
     public Poster(String title, int id, String genre) {
         this.title = title;
         this.id = id;
@@ -14,26 +15,32 @@ public class Poster {
     }
 
     public String getTitle() {
+
         return title;
     }
 
     public void setTitle(String title) {
+
         this.title = title;
     }
 
     public int getId() {
+
         return id;
     }
 
     public void setId(int id) {
+
         this.id = id;
     }
 
     public String getGenre() {
+
         return genre;
     }
 
     public void setGenre(String genre) {
+
         this.genre = genre;
     }
 
--- src/test/java/ru/netology/manager/FilmManagerTest.java
+++ src/test/java/ru/netology/manager/FilmManagerTest.java
@@ -6,6 +6,17 @@ import ru.netology.domain.Poster;
 import ru.netology.manager.FilmManager;
 
 public class FilmManagerTest {
+    Poster film1 = new Poster("Бладшот", 1, "боевик");
+    Poster film2 = new Poster("Вперед", 2, "мультфильм");
+    Poster film3 = new Poster("Отель Белград", 3, "комедия");
+    Poster film4 = new Poster("Джентельмены", 4, "боевик");
+    Poster film5 = new Poster("Человек-невидимка", 5, "ужасы");
+    Poster film6 = new Poster("Тролли.Мировой тур", 6, "мультфильм");
+    Poster film7 = new Poster("Номер один", 7, "комедия");
+    Poster film8 = new Poster("Брат", 8, "драма");
+    Poster film9 = new Poster("Один дома", 9, "комедия");
+    Poster film10 = new Poster("Зеленая миля", 10, "драма");
+    Poster film11 = new Poster("Сумерки", 11, "фантастика");
 
     @Test
     public void whenAddFilm() {
@@ -44,7 +55,7 @@ public class FilmManagerTest {
     }
 
     @Test
-    public void FindMinLast() {
+    public void FindFourLast() {
         FilmManager manager = new FilmManager();
 
         manager.addFilm("Film 1");
@@ -59,7 +70,39 @@ public class FilmManagerTest {
         Assertions.assertArrayEquals(expected, actual);
     }
 
+    @Test
+    public void FindLastMoreTen() {
+        FilmManager manager = new FilmManager(11);
+
+        manager.addFilm("Film 1");
+        manager.addFilm("Film 2");
+        manager.addFilm("Film 3");
+        manager.addFilm("Film 4");
+        manager.addFilm("Film 5");
+        manager.addFilm("Film 6");
+        manager.addFilm("Film 7");
+        manager.addFilm("Film 8");
+        manager.addFilm("Film 9");
+        manager.addFilm("Film 10");
+        manager.addFilm("Film 11");
+
+
+        String[] expected = {"Film 11", "Film 10", "Film 9", "Film 8", "Film 7", "Film 6", "Film 5", "Film 4", "Film 3", "Film 2", "Film 1"
+        };
+        String[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+
+
 
 }
+
+
+
+
+
+
 
 
